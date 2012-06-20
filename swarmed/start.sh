fc8587d0e52dc9fb62fc70ccfb269cc181d46063
Abram Hindle <abram.hindle@ualberta.ca>
2012-06-20 16:14:47 -0600

    more swarmed stuff

---
+++ swarmed/start.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+killall jackd
+killall mongrel2
+killall m2sh
+
+# this is so that later sudos are handled.
+sudo echo Sudo Up.
+sudo bash init-swarmed-network.sh
+sudo /etc/init.d/snsmasq restart
+cd ~/projects/mongrel2-musical-relay/
+urxvt +j -e bash jackd.sh &
+sleep 1
+urxvt +j -t 'MONGREL2' -e make start &
+urxvt +j -e perl 404.pl &
+urxvt +j -e perl 404.pl &
+urxvt +j -e perl http_harbinger.pl &
+urxvt +j -e perl http_harbinger.pl &
+sleep 1
+bash ~/projects/mongrel2-musical-relay/swarmed/instr.sh
+qjackctl &
