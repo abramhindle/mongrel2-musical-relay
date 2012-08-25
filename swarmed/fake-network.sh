f3a737e2aee31ff45e2985ef89e414e53e915981
Abram Hindle <abram.hindle@ualberta.ca>
2012-08-24 18:32:20 -0700

    missing files in prep for performance

---
+++ swarmed/fake-network.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+sudo ifconfig eth0 up 10.20.30.1 netmask 255.255.255.0
---
+++ swarmed/start-fake.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+killall jackd
+killall mongrel2
+killall m2sh
+
+# this is so that later sudos are handled.
+sudo echo Sudo Up.
+sudo bash fake.sh
+#sudo /etc/init.d/dnsmasq restart
+cd ~/projects/mongrel2-musical-relay/
+rm -rf ~/projects/mongrel2-musical-relay/run
+mkdir ~/projects/mongrel2-musical-relay/run
+urxvt +j -e bash jackd.sh &
+sleep 1
+urxvt +j -title 'MONGREL2' -e make start &
+urxvt +j -e perl 404.pl &
+urxvt +j -e perl 404.pl &
+urxvt +j -e perl http_harbinger.pl &
+urxvt +j -e perl http_harbinger.pl &
+sleep 1
+bash ~/projects/mongrel2-musical-relay/swarmed/instr.sh
+qjackctl &
